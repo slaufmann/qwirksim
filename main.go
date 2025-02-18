bb7fb80382030aac147338db38c8a9b232abc1b5
Stefan Laufmann <mail@stefanlaufmann.de>
2025-02-18 22:50:05 +0100

    doc: added comments to main.go

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+// Colour represents the colour of a [Tile].
 type Colour int
 
+// Values that are supported for [Colour].
 const (
 	UnknownColour Colour = iota
 	Red
@@ -16,8 +18,10 @@ const (
 	Purple
 )
 
+// Shape represents the shape of a [Tile].
 type Shape int
 
+// Values that are supported for [Shape].
 const (
 	UnknownShape Shape = iota
 	Circle
@@ -28,16 +32,21 @@ const (
 	Clover
 )
 
+// Values that serve as defaults.
+// TileQuantity represents how often a specific [Tile] is present in a queue of tiles.
+// TileCount represents the number of tiles in a queue.
 const (
 	TileQuantity = 3
 	TileCount    = 108
 )
 
+// Tile represents a single tile in the qwirkle game. Each tile has a colour and a shape.
 type Tile struct {
 	Colour Colour
 	Shape  Shape
 }
 
+// isColour returns a boolean indicating whether the given [Colour] c is a valid colour value.
 func (c Colour) isColour() bool {
 	switch c {
 	case Red:
@@ -57,6 +66,7 @@ func (c Colour) isColour() bool {
 	return false
 }
 
+// isShape returns a boolean indicating whether the given [Shape] s is a valid shape value.
 func (s Shape) isShape() bool {
 	switch s {
 	case Circle:
@@ -76,6 +86,8 @@ func (s Shape) isShape() bool {
 	return false
 }
 
+// createTileQueue returns a slice of [Tile] elements with a maximum size as given by the count
+// argument. Each tile can be contained multiple times but not more than the quantity argument.
 func createTileQueue(count, quantity int) []Tile {
 	var queue []Tile
 
